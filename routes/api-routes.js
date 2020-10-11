a9498e8dbb22f22f13c066521324c0317bc488e0
Sarah Seagraves <67175494+seagraving@users.noreply.github.com>
2020-10-11 11:52:24 -0500

    created routes to working page, now need to be able to delete

---
+++ routes/api-routes.js
@@ -0,0 +1,53 @@
+const db = require("../db/db.json");
+const fs = require("fs");
+const uuid = require("uuid");
+
+module.exports = function(app) {
+  app.get("/api/notes", function(req, res) {
+    res.send(db);
+  });
+
+  //create new note
+  app.post("/api/notes", function(req, res) {
+
+    let noteId = uuid();
+    let newNote = {
+      id: noteId,
+      title: req.body.title,
+      text: req.body.text
+    };
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+
+      const allNotes = JSON.parse(data);
+
+      allNotes.push(newNote);
+
+      fs.writeFile("./db/db.json", JSON.stringify(allNotes, null, 2), err => {
+        if (err) throw err;
+        res.send(db);
+        console.log("Note creation successful.")
+      });
+    });
+  });
+
+  //delete notes
+  app.delete("/api/notes/:id", (req, res) => {
+
+    let noteId = req.params.id;
+
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+      if (err) throw err;
+
+      const allNotes = JSON.parse(data);
+      const newAllNotes = allNotes.filter(note => note.id != noteId);
+
+      fs.writeFile("./db/db.json", JSON.stringify(newAllNotes, null, 2), err => {
+        if (err) throw err;
+        res.send(db);
+        console.log("Note deletion successful.")
+      });
+    });
+  });
+};
